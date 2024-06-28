fa67372562d6229754a9df66bb9b26a66fb61bf3
Security testing <118594933+w2h3@users.noreply.github.com>
2024-06-28 11:11:05 +0300

    Update lfr.php

--- api/lfr.php
+++ api/lfr.php
@@ -1,7 +1,6 @@
 <?php
 if (isset($_GET['file'])) {
     $file = $_GET['file'];
-    $filePath = __DIR__ . '/' . basename($file);
-    echo nl2br(htmlspecialchars(file_get_contents($filePath)));
+    echo nl2br(htmlspecialchars(file_get_contents($file)));
 }
 ?>
